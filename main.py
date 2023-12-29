49bc3845a889d0d15340f083b69d789938af8cbb
Alex Hergott <hergotta24@gmail.com>
2023-12-29 02:28:19 -0600

    Updated main to do count down. Also added default skip intro method. Working different way to grab image from screen.

--- main.py
+++ main.py
@@ -9,16 +9,29 @@ import services.SkipIntro as intro
 if __name__ == "__main__":
     save_folder = "screenshots"  # Folder path to save screenshots
     template_image_path = "OutroTemplate/template_image.png"  #Path to the template image you want to find
+    template_image_path2 = "OutroTemplate/laptop_template.png"  #Path to the template image you want to find
+
     click_x, click_y = 67, 1373  # Coordinates of Crunchry Roll Next Btn on my Monitor
-
-    # Start continuously taking screenshots until the image is recognized
-    # outro.continuously_take_screenshots(template_image_path, click_x, click_y)
-    # print("Move the mouse to see coordinates. Press Ctrl + C to stop.")
-    print_mouse_coordinates()
-    # #Define the start and end coordinates
-    # 2460
-    # start_x, start_y = 500, 1417
+    x, y = 138, 2027  # Coordinates of Crunchry Roll Next Btn on my laptop
+    x1, y1 = 297, 1417
+    x2, y2 = 580, 2115 # default intro place
+    x3, y3 = 623, 2114
     # end_x, end_y = 600, 1417
+    # start_x, start_y = 500, 1417
+    #outro.continuously_grab_image("Something")
 
-    # #Slide the mouse and take screenshots every 2 seconds
-    # slide_mouse_and_screenshot(start_x, start_y, end_x, end_y, duration=40, interval=30)
+    print("Starting Application.... Switch Tabs to Begin Binging!")
+    for x in range(8, 0, -1):
+        print("Starting auomation in " + str(x) + " seconds")
+        time.sleep(1)
+    while True:
+        intro.default_skip_intro(x3, y3)
+        outro.continuously_take_screenshots(template_image_path2, x, y)
+        time.sleep(2)
+
+
+    
+    
+
+
+   
