2b0df212bffd3c9f5f9649fb3529cfc75feec495
Alex Hergott <hergotta24@gmail.com>
2023-12-30 03:01:09 -0600

    Fixed the skip outro functionality. Also cleaned up the project

--- README.txt
+++
@@ -1,2 +0,0 @@
-Anime Skip To Next Episode Bot
--A bot the recognize the end of an Anime and automatically skips to the next episode. Also does image recognition to find the start of the next episode skipping past the into and the recap.
--- services/SkipOutro.py
+++ services/SkipOutro.py
@@ -30,16 +30,21 @@ def continuously_take_screenshots(template_path, click_x, click_y):
             print("\nNo screenshots taken.")
         print("Stopped.")
 
-def continuously_grab_image(template_image = "string"):
+def continuously_grab_images(template_path, click_x, click_y):
     last_time = time.time()
     while True:
-        screen = np.array(ImageGrab.grab(bbox=(824,243,1498,1047)))
+        screen = np.array(ImageGrab.grab(bbox=(1024,203,1698,1067)))
+        if find_image(template_path, screen):
+                print("Image recognized! Clicking...")
+                click_at_coordinates(click_x, click_y)
+                print("Clicked!")
+                return True
         print("Loop took {} seconds".format(time.time() - last_time))
         last_time = time.time()
-        cv2.imshow('window',cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
-        if cv2.waitKey(25) & 0xFF == ord('q'):
-            cv2.destroyAllWindows()
-            break
+        # cv2.imshow('window',cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
+        # if cv2.waitKey(25) & 0xFF == ord('q'):
+        #     cv2.destroyAllWindows()
+        #     break
             
             
 
@@ -48,15 +53,16 @@ def continuously_grab_image(template_image = "string"):
         
 # Click at specified coordinates
 def click_at_coordinates(x, y):
+    print(x, y)
     print(f"Clicking at coordinates: ({x}, {y})")
     click_coordinates(x, y)
 
-def find_image(template_path, screenshot_path):
-    template = cv2.imread(template_path, 0)
-    screenshot = cv2.imread(screenshot_path, 0)
+def find_image(template_path, screen):
+    template = cv2.imread(template_path)
+    grey_template = cv2.cvtColor(template, 6)
+    grey_screen = cv2.cvtColor(screen, 6)
 
-
-    result = cv2.matchTemplate(screenshot, template, cv2.TM_CCOEFF_NORMED)
+    result = cv2.matchTemplate(grey_screen, grey_template, cv2.TM_CCOEFF_NORMED)
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
 
     threshold = 0.6  # Adjust this threshold as needed
@@ -74,7 +80,7 @@ def take_screenshot(folder_path, timings):
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
     start_time = time.time()
-    screenshot = pyautogui.screenshot()
+    screenshot = pyautogui.screenshot(region=(824,243,1498,1047))
     end_time = time.time()
     timestamp = time.strftime("%Y%m%d%H%M%S")
     screenshot_path = os.path.join(folder_path, f"screenshot_{timestamp}.png")
