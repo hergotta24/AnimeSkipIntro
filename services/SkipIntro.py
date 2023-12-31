a9478d64c1dc74e8d4fb399e17a4200a1b73e2d8
Alex Hergott <hergotta24@gmail.com>
2023-12-31 01:22:27 -0600

    Started working on images grabbing for the timestamps

--- services/SkipIntro.py
+++ services/SkipIntro.py
@@ -1,5 +1,7 @@
 import pyautogui
 import time
+import numpy as np
+from PIL import ImageGrab
 
 def slide_mouse_and_screenshot(start_x, start_y, end_x, end_y, duration, interval=10):
     # Set the initial position of the mouse
@@ -42,3 +44,37 @@ def default_skip_intro(x, y):
         time.sleep(1)
         pyautogui.press('space')
         first_skip = False
+
+#TODO GET Image of timestamp and continously grab it.
+#TODO Install pytessoraact
+#TODO Methods for image recognition
+
+def find_title_card(start_x, start_y, end_x, end_y, duration, timestamp, start_time):
+     
+   
+    pyautogui.moveTo(start_x, start_y)
+    time.sleep(1)
+    pyautogui.moveTo(end_x, start_y, duration)
+
+    screen = np.array(ImageGrab.grab(bbox=(1024,203,1698,1067)))
+
+    
+
+    
+    current_x = start_x
+    while current_x < end_x:
+    # Define the region above the current mouse position to take a screenshot
+        screenshot_region = (current_x + 82, start_y - 30, 40, 18)
+    
+         # Take a screenshot of the defined region
+        screenshot = pyautogui.screenshot(region=screenshot_region)
+        screenshot.save(f"screenshot_{current_x}.png")  # Save the screenshot with a filename based on the current x-coordinate
+    
+        # Wait for 'interval' seconds
+        time.sleep(initerval)
+    
+         # Move to the next x-coordinate
+        current_x += distance // (duration * 10)  # Move 1/10th of the distance each time (adjust as needed)
+
+     
+    return "FUCK"
